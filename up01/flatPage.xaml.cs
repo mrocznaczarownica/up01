d21711f8193b535e34a39bedc71aa4520433e1a3
mrocznaczarownica <90793865+mrocznaczarownica@users.noreply.github.com>
2022-02-19 13:30:14 +0400

    a

---
+++ up01/flatPage.xaml.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace up01
+{
+    /// <summary>
+    /// Логика взаимодействия для flatPage.xaml
+    /// </summary>
+    public partial class flatPage : Page
+    {
+        public flatPage()
+        {
+            InitializeComponent();
+        }
+
+        private void adressHouse_PreviewTextInput(object sender, TextCompositionEventArgs e)
+        {
+            int val;
+            if (!Int32.TryParse(e.Text, out val) && e.Text != "-")
+            {
+                e.Handled = true;
+            }
+        }
+
+        private void adressHouse_PreviewKeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.Key == Key.Space)
+            {
+                e.Handled = true;
+            }
+        }
+    }
+}
